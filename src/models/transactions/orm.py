a5d1a78442ac53253db534cdcf23b785ccefcbce
Artem Kuznetsov <arteemkuznetsov@gmail.com>
2025-01-28 00:43:15 +0700

    alembic transaction value precision

--- src/models/transactions/orm.py
+++ src/models/transactions/orm.py
@@ -7,7 +7,7 @@ from src.db import Base
 class Transaction(Base):
     __tablename__ = 'transactions'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    value: Mapped[float] = mapped_column(Numeric, nullable=False)
+    value: Mapped[float] = mapped_column(Numeric(precision=10, scale=4), nullable=False)
     timestamp: Mapped[int] = mapped_column(BigInteger, nullable=False)
     type_id: Mapped[int] = mapped_column(BigInteger, ForeignKey('transtypes.id', ondelete='CASCADE'), nullable=False)
     type = relationship(
