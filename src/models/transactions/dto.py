50beca99ec1dae1dd66884bf10181299912031ca
Artem Kuznetsov <arteemkuznetsov@gmail.com>
2025-02-11 06:48:13 +0700

    fix

--- src/models/transactions/dto.py
+++ src/models/transactions/dto.py
@@ -4,5 +4,5 @@ from src.models.dto import PydanticBaseModel
 class TransactionView(PydanticBaseModel):
     value: float
     timestamp: int
-    order_id: str
+    order_id: str | None = None  # TODO: исправить после того как заполню поля order_id в таблице и сделаю поле NOT_NULL
     type_id: int
