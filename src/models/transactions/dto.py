8b913a1c4cfb7b2ec11e5777f143631793528cef
Artem Kuznetsov <arteemkuznetsov@gmail.com>
2025-02-11 17:38:58 +0700

    transaction detail info

--- src/models/transactions/dto.py
+++ src/models/transactions/dto.py
@@ -4,5 +4,9 @@ from src.models.dto import PydanticBaseModel
 class TransactionView(PydanticBaseModel):
     value: float
     timestamp: int
-    order_id: str | None = None  # TODO: исправить после того как заполню поля order_id в таблице и сделаю поле NOT_NULL
     type_id: int
+    order_id: str
+    quantity: float
+    fee: float
+    pair_id: int
+    balance: float
--- src/models/transactions/orm.py
+++ src/models/transactions/orm.py
@@ -11,8 +11,16 @@ class Transaction(Base):
     timestamp: Mapped[int] = mapped_column(BigInteger, nullable=False)
     order_id: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     type_id: Mapped[int] = mapped_column(BigInteger, ForeignKey('transtypes.id', ondelete='CASCADE'), nullable=False)
+    quantity: Mapped[float] = mapped_column(Numeric(precision=12, scale=6), nullable=False)
+    fee: Mapped[float] = mapped_column(Numeric(precision=18, scale=12), nullable=False)
+    pair_id: Mapped[int] = mapped_column(BigInteger, ForeignKey('pairs.id', ondelete='CASCADE'), nullable=False)
     type = relationship(
         'TransType',
         foreign_keys=[type_id],
         lazy='joined',
+    )
+    pair = relationship(
+        'Pair',
+        foreign_keys=[pair_id],
+        lazy='joined'
     )
